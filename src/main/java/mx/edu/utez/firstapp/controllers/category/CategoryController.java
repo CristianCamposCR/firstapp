408a49a787c599cd105e52dc379fe7e5fcf53e49
Cristian CR <20213tn003@utez.edu.mx>
2023-03-01 15:19:11 -0600

    cambios de permisos en mainSecurity

--- src/main/java/mx/edu/utez/firstapp/controllers/category/CategoryController.java
+++ src/main/java/mx/edu/utez/firstapp/controllers/category/CategoryController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api-fisrtapp/category")
+@RequestMapping("/api-firstapp/category")
 @CrossOrigin(origins = {"*"})
 public class CategoryController {
     @Autowired
--- src/main/java/mx/edu/utez/firstapp/security/MainSecurity.java
+++ src/main/java/mx/edu/utez/firstapp/security/MainSecurity.java
@@ -54,14 +54,21 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     @Override
     //este metodo proteje nuestrar rutas
     protected void configure(HttpSecurity http) throws Exception{//solo se puede acceder a este medodo desde security
-        http.cors().and().csrf().disable().authorizeRequests().antMatchers("/api-firstapp/auth/**", "/api-firstapp/contact/**")
-                .permitAll().antMatchers("/api-fisrtapp/category/*").permitAll()
-                .antMatchers(HttpMethod.POST, "/api-firstapp/user/").permitAll()
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api-firstapp/auth/**", "/api-firstapp/contact/**").permitAll()
+//                .antMatchers("/api-firstapp/category/*").permitAll() descomentar para dar acceso publico
+                .antMatchers(HttpMethod.POST,"/api-firstapp/user/").permitAll()
                 .antMatchers(HttpMethod.GET, "/api-firstapp/product/*").permitAll()
                 .antMatchers(HttpMethod.POST, "/api-firstapp/person/").permitAll()
-                .anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(entryPoint)
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(entryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(),
+                UsernamePasswordAuthenticationFilter.class);
                 //investigar cors y csrf
         //antMatchers() toda peticion tiene un path, y con esto las hacemos publicas
     }
